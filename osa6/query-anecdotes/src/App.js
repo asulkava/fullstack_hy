432897f0777c118dfe7861af2a5aecb3fb950edd
Anton Sulkava <anton.sulkava@aalto.fi>
2023-02-27 18:40:30 +0200

    osa6 parts 6.20-6.22

--- osa6/query-anecdotes/src/App.js
+++ osa6/query-anecdotes/src/App.js
@@ -1,19 +1,40 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from 'react-query' 
+import { getAnecdotes, updateAnecdote  } from './requests'
 
 const App = () => {
 
+  const queryClient = useQueryClient()
+
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    },
+  })
+
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
   }
 
-  const anecdotes = [
+
+  const result = useQuery(
+    'anecdotes', getAnecdotes, 
     {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+      retry: 1
+    }
+  )
+
+  if(result.isLoading) {
+    return <div>loading data...</div>
+  }
+  else if ( result.isError  ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
 
   return (
     <div>
